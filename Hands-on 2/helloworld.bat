d31da8ad4e4aaf4aa42328ea6894950bcc73b83f
Johana070500 <84247199+Johana070500@users.noreply.github.com>
2024-05-22 06:36:57 -0600

    Create helloworld.bat

---
+++ Hands-on 2/helloworld.bat
@@ -0,0 +1,19 @@
+@echo off
+
+::Imprimimos Hello World::
+echo Hello World
+
+::Listamos el contenido del directorio actual::
+dir
+
+::Creamos un directorio con el nombre "Test"::
+mkdir Test
+
+::Accedemos al directorio creado::
+cd Test
+
+::Listamos el contenido del directorio "Test"::
+dir
+
+::Pausamos la ejecucion para que nose cierre la ventana antes de tiempo::
+pause
